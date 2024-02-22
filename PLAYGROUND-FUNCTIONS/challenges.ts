734da86317a5f3d8737c857bcc8f877b236faece
Thalys Rocha Rodrigues <tgbr66@gmail.com>
2024-02-22 09:45:00 -0300

    feat: Implementa a função calcArea

--- PLAYGROUND-FUNCTIONS/challenges.ts
+++ PLAYGROUND-FUNCTIONS/challenges.ts
@@ -1,4 +1,6 @@
-// Desafio 1
-function compareTrue(value1: boolean, value2: boolean) {
-  return value1 === true && value2 === true;
-}
+
+// Desafio 2
+function calcArea(height: number, weight: number) {
+    return (height * weight) / 2;
+  }
+  
