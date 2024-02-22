6c1b20ffb47bb4acaec7ec4f95396918cab9b5b7
Thalys Rocha Rodrigues <tgbr66@gmail.com>
2024-02-22 09:46:07 -0300

    fix: Retoma a função compareTrue para o arquivo

--- PLAYGROUND-FUNCTIONS/challenges.ts
+++ PLAYGROUND-FUNCTIONS/challenges.ts
@@ -1,3 +1,8 @@
+// Desafio 1
+function compareTrue(value1: boolean, value2: boolean) {
+  return value1 === true && value2 === true;
+}
+
 
 // Desafio 2
 function calcArea(height: number, weight: number) {
